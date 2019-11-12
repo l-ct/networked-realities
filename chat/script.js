685714e5e2459abed751191a7221ecc146e1dba3
L-C <ltiar@risd.edu>
2019-11-11 19:19:06 -0500

    saving to  db

--- chat/script.js
+++ chat/script.js
@@ -1,31 +1,72 @@
 const socket = io('https://api.networkedrealiti.es/louis').connect();
 
-// first we're selected all the elements
 // querySelector() returns the first match of a CSS selector
 let form = document.querySelector('form');
-let main = document.querySelector('main');
-// using getElementById here because there are two input elements in the html
-// and I wanted to avoid confusion
-let input = document.getElementById('input');
-
-// do something when user clicks submit button or hits enter
-// both events trigger a form submit
+// When user clicks submit button or hits enter
+// a form submit event is triggered
 form.addEventListener('submit', event => {
-	// send input.value, in this case a string, to the server
-	// so it will broadcast to the other users
-	socket.emit('broadcast', input.value);
-	// in addition, add this add this to this user's "main" element
-	main.innerHTML = '<p>' + input.value + '</p>' + main.innerHTML;
-	// set the input field to nothing
-	input.value = '';
+	// using getElementById here because there are two input elements in the html
+	// and I wanted to avoid confusion
+	let input = document.getElementById('input');
+
+	// input.value is a getter and setter
+	if(input.value != ''){
+
+		// Send user input to the server.
+		// The 'insert' channel saves an object to a database
+		// in addition to broadcasting it to other connected users.
+		// We can't use primitive data types with 'insert'
+		// only objects, or arrays of objects.
+		socket.emit('insert', {
+			app: 'chat',
+			text: input.value
+		});
+
+		// function defined below
+		addToMain(input.value);
+
+		// set the input field to nothing
+		input.value = '';
+	}
+
 	// prevent the form from actually being submitted to the server
 	event.preventDefault();
 });
 
-// receiving data from server
-socket.on('broadcast', data => {
-	// data is a string, so we're going to wrap it in p tags
-	// and add it to the main element
-	// notice how I'm not using the += operator an you tell me why?
-	main.innerHTML = '<p>' + data + '</p>' + main.innerHTML;
+// receiving data from server using the same 'insert' channel
+socket.on('insert', data => {
+	// every time another user sends data to server
+	// current user adds it to their interface as well
+	// I'm using an if statemnt in case we've received info
+	if(data.app == 'chat'){
+		addToMain(data.text);
+	}
 });
+
+// now I'm passing an object in the find channel...
+// this will ensure that only data from this app is returned
+// otherwise we could get everything from the DB
+// and filter the returned data using if statements
+// passing an empty object as the second parameter
+// retrieves all the items in the database
+socket.emit('find', {app:'chat'}, data => {
+	console.log(data);
+	// data will be an array of saved objects
+	// using for loop to cycle through each one in order
+	for(let i=0; i<data.length; i++){
+		addToMain(data[i].text);
+	}
+});
+
+// takes a string
+// wraps it in p tags
+// and prepends it to the main element
+function addToMain(text){
+	// first we select the main element
+	let main = document.querySelector('main');
+	// Getting and setting the .innerHTML value
+	// allows us to manipulate its contents
+	// notice how I'm not using the += operator
+	// ... can you tell me why?
+	main.innerHTML = '<p>' + text + '</p>' + main.innerHTML;
+}
--- draw/script.js
+++ draw/script.js
@@ -14,24 +14,48 @@ window.addEventListener('click', event => {
 
 	// sending data to the server, in this example an object,
 	// to be broadcasted to the other users
-	socket.emit('broadcast', {
+	socket.emit('insert', {
 		// divide event's x and y coordinates
 		// by the window's width and height
 		// to get a relative position from 0 to 1
 		x: event.clientX / window.innerWidth,
-		y: event.clientY / window.innerHeight
+		y: event.clientY / window.innerHeight,
+		// adding an arbitrarily named 'app' key to this object
+		// in order to distinguish data sent from this app
+		// from others that will share the same database
+		app: 'draw'
 	});
 });
 
 
 // this is the part of the websocket that receives data from the
-socket.on('broadcast', data => {
+socket.on('insert', data => {
+	console.log(data);
 	// addDot is invoked again when data is received
 	// from the server
 	// we know that the data.x and data.y are from 0 to 1
 	// so we multiply by the window's width and height
-	// to get the absolute position
-	addDot(data.x * window.innerWidth, data.y * window.innerHeight);
+	// to get this user's absolute positions
+	addDot(
+		data.x * window.innerWidth,
+		data.y * window.innerHeight
+	);
+});
+
+// passing an object in the find channel...
+// this will ensure that only data from this draw app is returned
+// passing an empty object as the second parameter
+// retrieves all the items in the database
+socket.emit('find', {app:'draw'}, data => {
+	console.log(data);
+	// data will be an array of saved objects
+	// using for loop to cycle through each one
+	for(let i=0; i<data.length; i++){
+		addDot(
+			data[i].x * window.innerWidth,
+			data[i].y * window.innerHeight
+		);
+	}
 });
 
 
